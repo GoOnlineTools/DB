c3890d06d96a9c8fcd13736995b16de60e184098
Rahul Kumar Singh <37482171+rahulkumarsingh73690@users.noreply.github.com>
2023-06-21 14:47:58 +0000

    Pending changes exported from your codespace

---
+++ server/facts.js
@@ -0,0 +1,20 @@
+[  {
+    fact: 'The world’s oldest known pants were found in China and are over 3,000 years old.',
+    source: 'https://en.wikipedia.org/wiki/Trousers'
+  },
+  {
+    fact: 'Honey never spoils. You can eat 3,000-year-old honey.',
+    source: 'https://en.wikipedia.org/wiki/Honey'
+  },
+  {
+    fact: 'The average person walks the equivalent of five times around the world in their lifetime.',
+    source: 'https://en.wikipedia.org/wiki/Walking'
+  },
+  {
+    fact: 'The shortest war in history was between Britain and Zanzibar in 1896. Zanzibar surrendered after 38 minutes.',
+    source: 'https://en.wikipedia.org/wiki/Anglo-Zanzibar_War'
+  },
+  {
+    fact: 'The Earth has traveled more than 5,000 miles in the past 5 minutes.',
+    source: 'https://en.wikipedia.org/wiki/Earth'
+  },]
